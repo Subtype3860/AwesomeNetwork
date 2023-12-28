41c5977e4c1ab843936db9d4f3d5513e37426adc
Subtype3860 <113200110+Subtype3860@users.noreply.github.com>
2023-12-28 15:45:29 +1100

    Delete project

--- Models/ErrorViewModel.cs
+++
@@ -1,8 +0,0 @@
-namespace AwesomeNetwork.Models;
-
-public class ErrorViewModel
-{
-    public string? RequestId { get; set; }
-
-    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-}
