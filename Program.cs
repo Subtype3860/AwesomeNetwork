6e1ccc739b7a0c86176ced0d5d39211a9d140b1d
Subtype3860 <booravoi@gmail.com>
2024-01-01 17:23:10 +1100

    Обновление ветки BranchBackend. Создание maping. Создание Repository

--- Program.cs
+++ Program.cs
@@ -1,6 +1,45 @@
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using AwesomeNetwork.Models;
+using AwesomeNetwork;
+
 var builder = WebApplication.CreateBuilder(args);
+var connection = builder.Configuration.GetConnectionString("DefaultConnection");
+
+builder.Services
+    .AddDbContext<ApplicationDbContext>(options => options.UseSqlite(connection))
+    .AddUnitOfWork()
+    .AddIdentity<User, IdentityRole>(opts =>
+    {
+        opts.Password.RequiredLength = 5;
+        opts.Password.RequireNonAlphanumeric = false;
+        opts.Password.RequireLowercase = false;
+        opts.Password.RequireUppercase = false;
+        opts.Password.RequireDigit = false;
+    })
+    .AddEntityFrameworkStores<ApplicationDbContext>();
+
+// Add services to the container.
+builder.Services.AddRazorPages();
+
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Error");
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
+}
 
-app.Run();
+app.UseHttpsRedirection();
+app.UseStaticFiles();
+
+app.UseRouting();
+
+app.UseAuthentication();
+app.UseAuthorization();
+
+app.MapRazorPages();
+
+app.Run()
---
+++ ViewModels/Account/UserViewModel.cs
@@ -0,0 +1,9 @@
+﻿using AwesomeNetwork.Models;
+
+namespace AwesomeNetwork.ViewModels.Account
+{
+    public class UserViewModel(User user)
+    {
+        public User User { get; set; } = user;
+    }
+}
