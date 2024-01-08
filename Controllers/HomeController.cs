1ba6d941be1c2decf8e02d66b2d0c7704947ee64
Subtype3860 <booravoi@gmail.com>
2024-01-08 11:47:49 +1100

    Create controlers

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -1,31 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
 using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
 using AwesomeNetwork.Models;
+using AwesomeNetwork.Data.UnitofWork;
+using AutoMapper;
+using AwesomeNetwork.ViewModels.Account;
 
-namespace AwesomeNetwork.Controllers;
-
-public class HomeController : Controller
+namespace AwesomeNetwork.Controllers
 {
-    private readonly ILogger<HomeController> _logger;
-
-    public HomeController(ILogger<HomeController> logger)
+    public class HomeController : Controller
     {
-        _logger = logger;
-    }
+        private readonly ILogger<HomeController> _logger;
 
-    public IActionResult Index()
-    {
-        return View();
-    }
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
 
-    public IActionResult Privacy()
-    {
-        return View();
-    }
+        [Route("")]
+        [Route("[controller]/[action]")]
+        public IActionResult Index()
+        {
+            return View(new MainViewModel());
+        }
 
-    [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
-    public IActionResult Error()
-    {
-        return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        [Route("[action]")]
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
     }
 }
