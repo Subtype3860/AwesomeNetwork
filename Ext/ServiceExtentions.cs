70ec591417ee08979e21fd9b1617c6dd9b7ce12a
IvanBooravoi <booch74@ya.ru>
2024-01-10 17:51:59 +1100

    Работа над ошибками

---
+++ Ext/ServiceExtentions.cs
@@ -0,0 +1,26 @@
+﻿using AwesomeNetwork.Data.Repository;
+using AwesomeNetwork.Data.UnitOfWork;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace AwesomeNetwork.Ext
+{
+    public static class ServiceExtentions
+    {
+        public static IServiceCollection AddUnitOfWork(this IServiceCollection services)
+        {
+            services.AddScoped<IUnitOfWork, UnitOfWork>();
+
+            return services;
+        }
+
+        public static IServiceCollection AddCustomRepository<TEntity, IRepository>(this IServiceCollection services)
+                 where TEntity : class
+                 where IRepository : class, IRepository<TEntity>
+        {
+            services.AddScoped<IRepository<TEntity>, IRepository>();
+
+            return services;
+        }
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,74 @@
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using AutoMapper;
+using AwesomeNetwork.Data;
+using AwesomeNetwork.Data.Repository;
+using AwesomeNetwork.Ext;
+using AwesomeNetwork.Models.Users;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.AspNetCore.Http;
+
+
+namespace AwesomeNetwork
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var builder = WebApplication.CreateBuilder(args);
+
+
+            //БД
+            var connection = builder.Configuration.GetConnectionString("DefaultConnection");
+            var mapperConfig = new MapperConfiguration((v) =>
+            {
+                v.AddProfile(new MappingProfile());
+            });
+            var mapper = mapperConfig.CreateMapper();
+            builder.Services.AddSingleton(mapper);
+            builder.Services
+                .AddDbContext<ApplicationDbContext>(options => options.UseSqlServer(connection))
+                .AddUnitOfWork()
+                .AddCustomRepository<Message, MessageRepository>()
+                .AddCustomRepository<Friend, FriendsRepository>()
+                .AddIdentity<User, IdentityRole>(opts =>
+                   {
+                       opts.Password.RequiredLength = 5;
+                       opts.Password.RequireNonAlphanumeric = false;
+                       opts.Password.RequireLowercase = false;
+                       opts.Password.RequireUppercase = false;
+                       opts.Password.RequireDigit = false;
+                   })
+                .AddEntityFrameworkStores<ApplicationDbContext>();
+            builder.Services.AddControllersWithViews();
+            builder.Services.AddRazorPages();
+            // Add services to the container.
+            //builder.Services.AddControllersWithViews().AddRazorRuntimeCompilation();
+            var app = builder.Build();
+            // Configure the HTTP request pipeline.
+            if (!app.Environment.IsDevelopment())
+            {
+                app.UseExceptionHandler("/Home/Error");
+            }
+            app.UseHttpsRedirection();
+            const string cachePeriod = "0";
+            app.UseStaticFiles(new StaticFileOptions
+            {
+                OnPrepareResponse = ctx =>
+                {
+                    ctx.Context.Response.Headers.Append("Cache-Control", $"public, max-age={cachePeriod}");;
+                }
+            });
+            app.UseRouting();
+            app.UseAuthentication();
+            app.UseAuthorization();
+            app.MapControllerRoute(
+                name: "default",
+                pattern: "{controller=Home}/{action=Index}/{id?}");
+            app.Run();
+        }
+    }
+}
